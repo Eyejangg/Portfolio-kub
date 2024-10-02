340c33fc073aa43a64fbf4eac0775260ba33a609
Eyejangg <ptwptw1550@gmail.com>
2024-10-02 17:09:53 +0700

    Update

--- src/components/FeaturedBox.jsx
+++ src/components/FeaturedBox.jsx
@@ -6,66 +6,51 @@ const FeaturedBox = () => {
     <section className="featured-box" id="home">
       <div className="featured-text">
         <div className="featured-text-card">
-          <span>Phitharawat Ketmanee </span>
+          <span>Boonruksa Winanon</span>
         </div>
-
         <div className="featured-name">
           I'm <span className="typedText"></span>
         </div>
-
-        <div className="featured-text-info"></div>
-        <p>
-          เขียนประวัติเรา ทำงานเรื่องไหน ไบโอของเรา เขียนเรื่องอะไรเขียนไปลุย
-        </p>
-
+        <div className="featured-text-info">
+          <p>
+            Hello! My name is Boonruksa Winanon, and I am a software
+            Engineering. I am passionate about Javascript and have experience in
+            mySQL and UX/UI . I enjoy collaborating with others and am always
+            eager to learn and grow.
+          </p>
+        </div>
         <div className="featured-text-btn">
           <button className="btn">
-            Dowload CV <i className="uil uil-import"></i>
+            Download CV <i className="uil uil-import"></i>
           </button>
         </div>
-      </div>
-      <div className="social_icons">
-        <div className="icon">
-          <a href="http://www.instagram.com/se_npru">
-            <i className="uil uil-instagram-alt"></i>
-          </a>
+        <div className="social_icons">
+          <div className="icon">
+            <a href="https://www.instagram.com/o_obo70l?igsh=Ymk0Y3h5dHptcTc2">
+              <i className="uil uil-instagram-alt"></i>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://www.facebook.com/boonruksa.winanon.5?mibextid=LQQJ4d">
+              <i className="uil uil-facebook"></i>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://github.com/FAYRESU">
+              <i className="uil uil-github"></i>
+            </a>
+          </div>
         </div>
       </div>
-
-      <div className="social_icons">
-        <div className="icon">
-          <a href="http://www.instagram.com/se_npru">
-            <i className="uil uil-instagram-alt"></i>
-          </a>
-        </div>
-      </div>
-
-      <div className="social_icons">
-        <div className="icon">
-          <a href="http://www.instagram.com/se_npru">
-            <i className="uil uil-instagram-alt"></i>
-          </a>
-        </div>
-      </div>
-
-      <div className="social_icons">
-        <div className="icon">
-          <a href="http://www.instagram.com/se_npru">
-            <i className="uil uil-instagram-alt"></i>
-          </a>
-        </div>
-      </div>
-
       <div className="featured-image">
         <div className="image">
           <img src={avatar} alt="avatar" />
         </div>
       </div>
-
       <div className="scroll-icon-box">
         <a href="#about" className="scroll-btn">
           <i className="uil uil-mouse-alt"></i>
-          <p Scroll Down></p>
+          <p>Scroll Down</p>
         </a>
       </div>
     </section>
